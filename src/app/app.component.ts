90dc86a8adb20e77f39d218d738de3ff62074662
CORPORATE\pab0331 <peter.banko@daugherty.com>
2018-04-06 23:16:37 -0500

    Peter - Setup MEAN Stack Added Mongodb, Express.js, and Node.js You can now pull data from the Mongodb RESTfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  visitors: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('/visitor').subscribe(data => {
+      this.visitors = data;
+    });
+  }
 }
