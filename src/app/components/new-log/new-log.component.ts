a7ac7202138542e30c642561fa5d84aa10bc2996
Melissa Schrum <melissaschrum@gmail.com>
2018-04-07 23:34:59 -0500

    Chnged some things

--- src/app/components/new-log/new-log.component.ts
+++ src/app/components/new-log/new-log.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Visit } from './../../../classes/visit';
 import { Visitor } from './../../../classes/visitor';
+
+import { ActionContext } from '../../models/strategy/action-context';
+import { SpeechRecognizerService } from '../../services/speech-recognizer.service';
+import { SpeechNotification } from '../../models/speech-notification';
+import { SpeechError } from '../../models/speech-error';
 
 
 @Component({
@@ -17,12 +22,21 @@ export class NewLogComponent implements OnInit {
   visitor = new Visitor;
   visitors: Visitor[];
 
-  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
+  recognizing = false;
+  notification: string;
+  currentLanguage = 'en-US';
+  actionContext: ActionContext = new ActionContext();
+
+  constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient,
+    private changeDetector: ChangeDetectorRef, private speechRecognizer: SpeechRecognizerService) { }
 
   ngOnInit() {
     this.http.get('/visitor').subscribe((data: Visitor[]) => {
       this.visitors = data;
     });
+    this.speechRecognizer.initialize(this.currentLanguage);
+    this.initRecognition();
+    this.notification = null;
   }
 
   saveVisit(data) {
@@ -49,5 +63,66 @@ export class NewLogComponent implements OnInit {
           console.log(err);
         }
       );
+  }
+
+  startButton(event) {
+    if (this.recognizing) {
+      this.speechRecognizer.stop();
+      return;
+    }
+    this.speechRecognizer.start(event.timeStamp);
+  }
+
+  private initRecognition() {
+    this.speechRecognizer.onStart()
+      .subscribe(data => {
+        this.recognizing = true;
+        this.detectChanges();
+      });
+
+    this.speechRecognizer.onEnd()
+      .subscribe(data => {
+        this.recognizing = false;
+        this.detectChanges();
+        this.notification = null;
+      });
+
+    this.speechRecognizer.onResult()
+      .subscribe((data: SpeechNotification) => {
+        const message = data.content.trim();
+        if (data.info === this.visit.comments && message.length > 0) {
+          this.visit.comments = `${this.visit.comments}\n${message}`;
+          this.actionContext.processMessage(message, this.currentLanguage);
+          this.detectChanges();
+          this.actionContext.runAction(message, this.currentLanguage);
+        }
+      });
+
+    this.speechRecognizer.onError()
+      .subscribe(data => {
+        switch (data.error) {
+          case SpeechError.BLOCKED:
+          case SpeechError.NOT_ALLOWED:
+            this.notification = `Cannot run the demo.
+            Your browser is not authorized to access your microphone. Verify that your browser has access to your microphone and try again.
+            `;
+            break;
+          case SpeechError.NO_SPEECH:
+            this.notification = `No speech has been detected. Please try again.`;
+            break;
+          case SpeechError.NO_MICROPHONE:
+            this.notification = `Microphone is not available. Plese verify the connection of your microphone and try again.`;
+            break;
+          default:
+            this.notification = null;
+            break;
+        }
+        this.recognizing = false;
+        this.detectChanges();
+      });
+  }
+
+  detectChanges() {
+    this.changeDetector.detectChanges();
   }
 }
