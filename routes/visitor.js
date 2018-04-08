8654028acee7bdd64954aeb77baca29d5a8a9693
Matt Huberty <matthewhuberty77@gmail.com>
2018-04-08 00:45:57 -0500

    Back in working order, I think.

--- routes/visitor.js
+++ routes/visitor.js
@@ -7,19 +7,20 @@ const noble = require('noble');
 const devices = [];
 let backendVisitors;
 let knownBids = [];
+let scanResetTime = 60000;
 
 let getUpdatedVisitors = () => {
-  Visitor.find(function(err, visitors) {
+  Visitor.find(function (err, visitors) {
     if (err) return next(err);
     backendVisitors = visitors;
     visitors.forEach(visitor => {
       let insensitiveBid = visitor.bid.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
       if (knownBids.indexOf(insensitiveBid) === -1) {
         knownBids.push(insensitiveBid);
-        console.log(`Adding ${insensitiveBid} to knownBids`);
+        //console.log(`Adding ${insensitiveBid} to knownBids`);
       }
     });
-    console.log(knownBids);
+    // console.log(knownBids);
   })
 }
 getUpdatedVisitors();
@@ -29,18 +30,19 @@ setInterval(() => {
 }, 4000);
 
 
-/***************** BLUETOOTH SCANNING AND LOGIC *****************/ 
+/***************** BLUETOOTH SCANNING AND LOGIC *****************/
 
 noble.startScanning();
-noble.on('discover', function(device) {
+noble.on('discover', function (device) {
+  // console.log(device.advertisement.localName);
   if (device.advertisement.localName) {
     let deviceName = device.advertisement.localName.toString().toLowerCase().trim().replace(/[^a-zA-Z0-9]/g, "");
     let deviceObject = {};
     deviceObject[deviceName] = device.advertisement;
     devices.push(deviceObject);
-    console.log("Found " + deviceName);
+    //console.log("Found " + deviceName);
     if (knownBids.indexOf(deviceName) >= 0) {
-      console.log('FOUND SOMEONE I RECOGNIZE!! : ' + deviceName);
+      //console.log('FOUND SOMEONE I RECOGNIZE!! : ' + deviceName);
       backendVisitors.forEach(visitor => {
         console.log(`Visitor BID is : ${visitor.bid}`)
         let insensitiveBid = visitor.bid.toString().toLowerCase().trim().replace(/[^a-zA-Z0-9]/g, "");
@@ -48,13 +50,13 @@ noble.on('discover', function(device) {
           let matchedVisitor = visitor;
           let visitDate = Date.now();
           let visit = {
-                        user: {name: matchedVisitor.name, id: matchedVisitor._id},
-                        uploaded_date: visitDate,
-                        comments: 'Bluetooth Check-In'
-                      }
+            user: { name: matchedVisitor.name, id: matchedVisitor._id },
+            uploaded_date: visitDate,
+            comments: 'Bluetooth Check-In'
+          }
           matchedVisitor.visits.push(visit);
 
-          Visitor.findByIdAndUpdate(matchedVisitor._id, matchedVisitor, function(err, post) {
+          Visitor.findByIdAndUpdate(matchedVisitor._id, matchedVisitor, function (err, post) {
             if (err) {
               return next(err);
             }
@@ -62,16 +64,19 @@ noble.on('discover', function(device) {
           });
         }
       });
-     } else {
-       console.log(deviceName + " did not match any of: " + knownBids);
-     }
-   }
- })
+    } else {
+      console.log(deviceName + " did not match any of: " + knownBids);
+    }
+  }
+})
 
-/****************************************************************/ 
+
+
+
+/****************************************************************/
 
 /* GET ALL VisitorS */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   Visitor.find(function (err, products) {
     if (err) return next(err);
     res.json(products);
@@ -79,7 +84,7 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET SINGLE Visitor BY ID */
-router.get('/:id', function(req, res, next) {
+router.get('/:id', function (req, res, next) {
   Visitor.findById(req.params.id, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -87,12 +92,12 @@ router.get('/:id', function(req, res, next) {
 });
 
 // this isn't working just yet. TODO: add jade and set viewengine
-router.get('/devices', function(req, res, next) {
+router.get('/devices', function (req, res, next) {
   res.json(JSON.stringify(devices));
 })
 
 /* SAVE Visitor */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   Visitor.create(req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -100,7 +105,7 @@ router.post('/', function(req, res, next) {
 });
 
 /* UPDATE Visitor */
-router.put('/:id', function(req, res, next) {
+router.put('/:id', function (req, res, next) {
   Visitor.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
@@ -108,7 +113,7 @@ router.put('/:id', function(req, res, next) {
 });
 
 /* DELETE Visitor */
-router.delete('/:id', function(req, res, next) {
+router.delete('/:id', function (req, res, next) {
   Visitor.findByIdAndRemove(req.params.id, req.body, function (err, post) {
     if (err) return next(err);
     res.json(post);
